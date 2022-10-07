f333965d7b8a78c61aaaad831aef1f683a30b5b1
Peter Lund <112506867+CheetoLord@users.noreply.github.com>
2022-10-07 16:02:22 -0400

    Update part2.cpp

--- 2016/template/part2.cpp
+++ 2016/template/part2.cpp
@@ -1,11 +1,15 @@
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+
+    ifstream input("input.txt");
     
     cout << "Hello world! - part 2\n";
 
+    input.close();
     return 0;
 }
